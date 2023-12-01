5cfff92e58249fcb763924d6a6429916bb637cde
Ansanat <Ansanat228@gmail.com>
2023-12-02 01:13:45 +0700

    add specialist card

--- src/pages/v-specialists-page.vue
+++ src/pages/v-specialists-page.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="v-specialists-page">
-    <p>Servasdasdasdice</p>
+    <div class="specialist-card">
+    </div>
+    <div class="specialist-card">
+    </div>
+    <div class="specialist-card">
+    </div>
+    <div class="specialist-card">
+    </div>
+    <div class="specialist-card">
+    </div>
+    <div class="specialist-card">
+    </div>
+    <div class="specialist-card">
+    </div>
+    <div class="specialist-card">
+    </div>
   </div>
 </template>
 
@@ -13,5 +28,22 @@ export default {
 </script>
 
 <style>
-
+.specialist-card{
+  box-sizing: border-box;
+  overflow: hidden;
+  cursor: pointer;
+  display: inline-block;
+  border-radius: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 40px;
+  height: 300px;
+  width: 300px;
+  background-color: white;
+  box-shadow: 3px 3px 10px gray;
+}
+.specialist-card:hover{
+  border: 2px solid #358c21;
+  box-shadow: 3px 3px 10px #b9ecad;
+}
 </style>
